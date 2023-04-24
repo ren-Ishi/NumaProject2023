86de9ae4abcdc60d9d52f25b76d8b6ce3cc06676
Tenn-dazo <tenn.0619acc@gmail.com>
2023-04-24 17:05:21 +0900

    font test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-俺が仙石俺も仙石
+window.onload = function () {
+  //ロード画面_全体
+  const load = document.getElementById('loading');
+  load.classList.add('loaded');
+
+  //ロード画面_%
+  let count = 0;
+  const countUp = () =>{
+    document.getElementById('percent').textContent = 'Now loading...  ' + (++count)+'%';
+    console.log(count);
+  }
+  const intervalId = setInterval(() =>{
+    countUp();
+    if(count > 99){
+      clearInterval(intervalId);
+    }}, 50);
+}
