1706edf1d1f216ed7737485da40de691d3569d0e
Tenn-dazo <tenn.0619acc@gmail.com>
2023-04-27 18:18:41 +0900

    BAKEMONOホバー&ピックアップ完了

--- js/main.js
+++ js/main.js
@@ -5,13 +5,30 @@ window.onload = function () {
 
   //ロード画面_%
   let count = 0;
-  const countUp = () =>{
-    document.getElementById('percent').textContent = 'Now loading...  ' + (++count)+'%';
+  const countUp = () => {
+    document.getElementById('percent').textContent = 'Now loading...  ' + (++count) + '%';
     console.log(count);
   }
-  const intervalId = setInterval(() =>{
+  const intervalId = setInterval(() => {
     countUp();
-    if(count > 99){
+    if (count > 99) {
       clearInterval(intervalId);
-    }}, 50);
+    }
+  }, 50);
+}
+
+var img, txt;
+
+function btn01() {
+  img = document.getElementById("pickup_img");
+  txt = document.getElementById("pickup_txt");
+  img.src = "about/images/kekusun.png";
+  txt.textContent = "けくすんだお。つぼから出られないお…";
+}
+
+function btn10() {
+  img = document.getElementById("pickup_img");
+  txt = document.getElementById("pickup_txt");
+  img.src = "about/images/jump.PNG";
+  txt.textContent = "余命10秒のひんしきんぎょ。救いようがない。";
 }
