48f4d6467bb8261e60371adfbadd06f5289c8478
Sengoku-rem <103261281+Sengoku-rem@users.noreply.github.com>
2023-05-03 15:53:33 +0900

    キャラ絵をadd

--- js/main.js
+++ js/main.js
@@ -22,71 +22,71 @@ var img, txt;
 function btn01() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
-  txt.innerHTML = "けくすん<br>けくすんだお。つぼから出られないお…。沼でもあるお。";
+  img.src = "about/images/members/numa.png";
+  txt.innerHTML = "沼准教授<br>こんなんになっちゃいました。";
 }
 
 function btn02() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/mai.png";
   txt.innerHTML = "M A I ちゃん<br>V t u b e r 担当。激辛に取りつかれてしまい、日常的にエンタメ感覚で自己犠牲をする。";
 }
 
 function btn03() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/patapata.png";
   txt.innerHTML = "パタパタ<br>全肯定担当。常に作業し有給で地球を守る恐竜。先日ポ◯モンになった。";
 }
 
 function btn04() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/onzo.png";
   txt.innerHTML = "御曹司<br>外交担当。知り合いが多すぎて選挙に出ているんじゃないかと疑う。先日ポケモ◯にされかけた。";
 }
 
 function btn05() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/komikomi.png";
   txt.innerHTML = "コミコミ<br>ファンサ担当。ジンチを超える程よく食べてよく飲む。高校生の時からポケ◯ン。";
 }
 
 function btn06() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/sngk.png";
   txt.innerHTML = "仙茶<br>宗教担当。一般的になんか苦ぇなと感じる食品を好む。みんなを引きずってプロジェクトを結成した張本人。";
 }
 
 function btn07() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/ukigaichan.png";
   txt.innerHTML = "エビ焼売<br>対先生担当。ネギを欲しているように見えるが、多分本当に欲しいのは原石。";
 }
 
 function btn08() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/mayu.png";
   txt.innerHTML = "まゆ様<br>ﾈ ｷﾞ ﾎ ｽ ｨ の秘書担当。班活動が終わった時にはなぜか班員が従者になっている。";
 }
 
 function btn09() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/kekusun.png";
+  img.src = "about/images/members/yu.png";
   txt.innerHTML = "ゆ<br>壁担当。一回へばりつくと百均のシールくらいはがれない。最近ツボ男になった。";
 }
 
 function btn10() {
   img = document.getElementById("pickup_img");
   txt = document.getElementById("pickup_txt");
-  img.src = "about/images/jump.PNG";
-  txt.innerHTML = "、<br>余命１０秒のひんしきんぎょ。救いようがない。名前を読んでみよう。";
+  img.src = "about/images/members/oten.png";
+  txt.innerHTML = "、<br>3秒で忘れる。救いようがない。名前を読んでみよう。";
 }
 
 function back(){
